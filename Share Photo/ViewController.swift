9534d34b9be5fb24f0b6fe6cd27ba06a10b4bb5f
yukachilov <60588319+yukachilov@users.noreply.github.com>
2020-02-24 21:19:37 +0900

    Initial Commit

---
+++ Share Photo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Share Photo
+//
+//  Created by 安部学 on 2020/02/24.
+//  Copyright © 2020 Manabu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
